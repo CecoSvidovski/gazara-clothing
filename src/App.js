6c4c0c267f48c5075900352b6d368dd0d44ed7db
Ceco Svidovski <ceco_svidovski@abv.bg>
2022-08-05 09:33:09 +0300

    Add Navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import Directory from './components/Directory';
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from './pages/Navigation';
+import Home from './pages/Home';
+
+const Shop = () => <h1>I am the shop page</h1>;
 
 const App = () => {
   return (
-    <Directory />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/pages/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import { Outlet, Link } from 'react-router-dom';
+
+import './Navigation.scss';
+
+const Navigation = () => (
+  <>
+    <div className='nav'>
+      <div className='logo-container'>
+        <Link className='logo' to='/'>Gazara</Link>
+      </div>
+      <div className='nav-links-container'>
+        <Link className='nav-link' to='/shop'>
+          Shop
+        </Link>
+      </div>
+    </div>
+    <Outlet />
+  </>
+);
+
+export default Navigation;
